0ecf54dda77f98280d2b3f2cabf77a0a3f0a1fb8
Aasim Syed <${GITHUB_ACTOR}@users.noreply.github.com>
2025-02-23 22:48:48 -0500

    new init commit, restored w/ API

--- src/pages/api.tsx
+++ src/pages/api.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import BrowserOnly from '@docusaurus/BrowserOnly';
+import useIsBrowser from '@docusaurus/useIsBrowser';
 
 function SwaggerUI() {
-  return (
-    <BrowserOnly>
-      {() => (
-        <iframe
-          src="http://18.188.102.128:5000/apidocs/"
-          className="swagger-frame"
-          title="API Documentation"
-        />
-      )}
-    </BrowserOnly>
-  );
+  const isBrowser = useIsBrowser();
+  
+  return isBrowser ? (
+    <iframe
+      src="http://18.188.102.128:5000/apidocs/"
+      className="swagger-frame"
+      title="API Documentation"
+    />
+  ) : null;
 }
 
 export default function APIDoc(): React.ReactElement {
   return (
     <Layout
       title="API Documentation"
-      description="Thunder Buddy API Documentation">
+      description="Thunder Buddy API Documentation"
+      wrapperClassName="apiPage">
       <SwaggerUI />
     </Layout>
   );
