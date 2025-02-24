0b0e31a1be075c7377dd6bf29746f6067fdb3e00
Aasim Syed <${GITHUB_ACTOR}@users.noreply.github.com>
2025-02-24 09:38:09 -0500

    removed build/ and edited .gitignore, config

--- build/assets/js/aba21aa0.8a0f5820.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkthunder_buddy=self.webpackChunkthunder_buddy||[]).push([[742],{7093:u=>{u.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   organizationName: 'ThunderBuddyWeather', // Usually your GitHub org/user name.
   projectName: 'thunder-buddy-docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
