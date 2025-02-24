95d6d5fea672610740f70d049717e1b446a0278e
Aasim Syed <${GITHUB_ACTOR}@users.noreply.github.com>
2025-02-24 08:23:42 -0500

    build 1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,20 +10,20 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Thunder Buddy',
+  tagline: 'Thunder Buddy Documentation',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://thunderbuddyweather.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/thunder-buddy-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'thunderbuddyweather', // Usually your GitHub org/user name.
+  projectName: 'thunder-buddy-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -76,9 +76,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Thunder Buddy',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Thunder Buddy Logo',
           src: 'img/logo.svg',
         },
         items: [
